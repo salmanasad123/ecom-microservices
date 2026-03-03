6b5fb527a4c8f1a7bfade84471d760b8c35973c7
BH20584 <salman.asad@macys.com>
2026-03-03 16:41:17 +0500

    logging filter

---
+++ gateway/src/main/java/com/ecommerce/gateway/LoggingFilter.java
@@ -0,0 +1,25 @@
+package com.ecommerce.gateway;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+// Filter to just log the on-going request
+@Component
+public class LoggingFilter implements GlobalFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        // {} is parameterized log message
+        logger.info("Incoming request to : {}", exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
+}
